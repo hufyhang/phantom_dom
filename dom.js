96b772b178356511d769a8d6ac308bf8967f30e2
Feifei HANG <l.daniel.hung@gmail.com>
2014-01-14 17:55:31 +0000

    Corrected code indentation

--- dom.js
+++ dom.js
@@ -4,61 +4,61 @@ var _dom = (function() {
     return {
         grab: function(_args, callback) {
 
-var page = require('webpage').create();
+            var page = require('webpage').create();
 
-if(_args.url === undefined || _args.dom === undefined) {
-    callback(false);
-    return;
-}
-
-var url = _args.url,
-    dom = _args.dom;
-
-page.onConsoleMessage = function(msg) {
-    callback(msg);
-};
-
-page.open(url, function(status) {
-    if(status === 'success') {
-        page.injectJs("jquery.min.js");
-        page.evaluate(function(url, dom) {
-            if($(dom).length === 0) {
-                console.log("No match!!!");
+            if(_args.url === undefined || _args.dom === undefined) {
+                callback(false);
                 return;
             }
 
-            var buffer = "{";
-            buffer += "\"url\" : \"" + url.replace(/\"/g, "'") + "\", ";
-            buffer += "\"dom\" : \"" + dom + "\", ";
-            buffer += "\"data\": [";
+            var url = _args.url,
+            dom = _args.dom;
 
-            $.each($(dom), function() {
-                buffer += "{";
-                //get innerHTML
-                var text = $(this).html();
-                text = text.replace(/\"/g, "&quot;");
-                text = text.replace(/\n/g, " ");
-                buffer += "\"_\" : \"" + text + "\", ";
+            page.onConsoleMessage = function(msg) {
+                callback(msg);
+            };
 
-                //get attributes
-                $.each(this.attributes, function() {
-                    if(this.specified) {
-                        buffer += "\"" + this.name + "\" : \"" + this.value + "\", ";
-                    }
-                });
+            page.open(url, function(status) {
+                if(status === 'success') {
+                    page.injectJs("jquery.min.js");
+                    page.evaluate(function(url, dom) {
+                        if($(dom).length === 0) {
+                            console.log("No match!!!");
+                            return;
+                        }
 
-                buffer = buffer.slice(0, -2);
-                buffer += "}, ";
+                        var buffer = "{";
+                        buffer += "\"url\" : \"" + url.replace(/\"/g, "'") + "\", ";
+                        buffer += "\"dom\" : \"" + dom + "\", ";
+                        buffer += "\"data\": [";
+
+                        $.each($(dom), function() {
+                            buffer += "{";
+                            //get innerHTML
+                            var text = $(this).html();
+                            text = text.replace(/\"/g, "&quot;");
+                            text = text.replace(/\n/g, " ");
+                            buffer += "\"_\" : \"" + text + "\", ";
+
+                            //get attributes
+                            $.each(this.attributes, function() {
+                                if(this.specified) {
+                                    buffer += "\"" + this.name + "\" : \"" + this.value + "\", ";
+                                }
+                            });
+
+                            buffer = buffer.slice(0, -2);
+                            buffer += "}, ";
+                        });
+
+
+                        buffer = buffer.slice(0, -2);
+                        buffer += "]}";
+                        console.log(buffer);
+                    }, url, dom);
+                    page.close();
+                }
             });
-
-            
-            buffer = buffer.slice(0, -2);
-            buffer += "]}";
-            console.log(buffer);
-        }, url, dom);
-        page.close();
-    }
-});
 
 
         }
